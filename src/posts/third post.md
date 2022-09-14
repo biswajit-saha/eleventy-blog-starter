b1e8c3ff740dd32e2dccad84e9136351926af2af
Biswajit Saha <gbjsolution@gmail.com>
2022-09-14 22:51:36 +0530

    Improved typography for content

--- src/posts/third post.md
+++ src/posts/third post.md
@@ -5,7 +5,7 @@ date: 2021-10-03
 tags: ['Getting started', 'HTML CSS', 'programming']
 ---
 
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint numquam ipsum laboriosam, totam similique recusandae inventore placeat aspernatur soluta sequi hic quas explicabo perspiciatis vitae quis, mollitia porro deleniti! Similique sint neque temporibus.
+Lorem ipsum dolor sit amet [consectetur](https://example.com) adipisicing elit. Sint numquam ipsum laboriosam, totam similique recusandae inventore placeat aspernatur soluta sequi hic quas explicabo perspiciatis vitae quis, mollitia porro deleniti! Similique sint neque temporibus.
 
 
 > Ssoluta placeat cumque deleniti doloribus nihil deserunt obcaecati? Quam laudantium dicta voluptate ipsum provident cupiditate at nemo.
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,41 @@
+const { colors } = require('tailwindcss/defaultTheme');
 const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    mode: 'jit',
-    purge: [
-        './src/**/*.{njk, html, md}',
-    ],
-    darkMode: 'class', // or 'media' or 'class'
-    theme: {
-        extend: {
-            fontFamily: {
-                sans: ['Inter var', ...defaultTheme.fontFamily.sans],
-            },
-            lineHeight: {
-                '12': '3rem',
-            },
-            maxWidth: {
-                '1/2': '50%',
-            }
-        },
-    },
-    variants: {
-        extend: {
-            visibility: ['hover', 'focus', 'group-hover', 'group-focus'],
-        }
-    },
-    plugins: [
-        require('@tailwindcss/typography'),
-    ],
-}
+	mode: 'jit',
+	purge: ['./src/**/*.{njk, html, md}'],
+	darkMode: 'class', // or 'media' or 'class'
+	theme: {
+		extend: {
+			fontFamily: {
+				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+			},
+			lineHeight: {
+				12: '3rem',
+			},
+			maxWidth: {
+				'1/2': '50%',
+			},
+			typography: (theme) => ({
+				DEFAULT: {
+					css: {
+						a: {
+							'&:hover': {
+								color: theme('colors.red.500'),
+							},
+						},
+                        blockquote: {
+                            'border-left-color': theme('colors.red.500'),
+                        }
+					},
+				},
+			}),
+		},
+	},
+	variants: {
+		extend: {
+			visibility: ['hover', 'focus', 'group-hover', 'group-focus'],
+		},
+	},
+	plugins: [require('@tailwindcss/typography')],
+};
